b18a37520f1213517d9af797392bbd179583cfd4
Nhan Phan <nphanacc@gmail.com>
2021-10-11 10:43:37 -0700

    Install and verify Ant Design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-
+import './AntDesign.css';
+import { DatePicker } from "antd";
 
 function App() {
-  return (
-    <div >
-     App js
-    </div>
-  );
+	return (
+		<div>
+			<DatePicker />
+			App js
+		</div>
+	);
 }
 
 export default App;
