0c4504970f8a7b8c1e2d8a4bc2ca562b17bcc94b
Nhan Phan <nphanacc@gmail.com>
2021-10-11 13:06:44 -0700

    Show pop up Modal

--- src/Components/DeleteAccountButton.js
+++ src/Components/DeleteAccountButton.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Styles/AntDesign.css";
-import { Button } from "antd";
+import { Button, Modal } from "antd";
 
 const DeleteAccountButton = () => {
+	const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+
 	return (
 		<div>
-			<Button type="danger">Delete Account</Button>
+			<Button type="danger" onClick={showModal}>
+				Delete Account
+			</Button>
+      
+      <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
 		</div>
 	);
 };
