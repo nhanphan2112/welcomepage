0b8d85309bb71a0a350924fed3aa20ed0d3714f0
Nhan Phan <nphanacc@gmail.com>
2021-10-11 14:48:36 -0700

    Update Icon and Title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-// Need to comment line 3 and 4 out when complete application
-import "./Styles/AntDesign.css";
-// import { DatePicker } from "antd";
+
 
 import Welcome from "./Components/Welcome";
 
@@ -9,7 +7,6 @@ function App() {
 	return (
 		<div>
 			<Welcome />
-			{/* <DatePicker /> */}
 		</div>
 	);
 }
